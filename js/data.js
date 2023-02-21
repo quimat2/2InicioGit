239a6194861faa06b1b484a5ef2176e746e9ce10
Hector <quimat2@hotmail.com>
2023-02-21 00:38:07 -0600

    final

---
+++ js/data.js
@@ -0,0 +1,31 @@
+const songs = [
+    {
+        name: 'Hola pez con cola',
+        path: '../music/Song 1.mp3',
+        artist: 'Hector',
+        cover: '../images/cover 1.png'
+    },
+    {
+        name: 'Soy ese',
+        path: '../music/Song 2.mp3',
+        artist: 'Jair',
+        cover: '../images/cover 2.png'
+    },
+    {
+        name: 'El payaso del rodeo',
+        path: '../music/Song 3.mp3',
+        artist: 'Cartel de Santa',
+        cover: '../images/cover 3.png'
+    },
+    {
+        name: 'Humildad',
+        path: '../music/Song 4.mp3',
+        artist: 'MrBeast',
+        cover: '../images/cover 4.png'
+    },{
+        name: 'El juego',
+        path: '../music/Song 5.mp3',
+        artist: 'Tri Line',
+        cover: '../images/cover 5.png'
+    },
+]
